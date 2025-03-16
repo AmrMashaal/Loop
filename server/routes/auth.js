e1dd36514f5dd82b8a49d538bf33171710a4e02e
amrmashaal <amrhossammashal@gmail.com>
2025-03-16 20:19:57 +0200

    Updated project with latest changes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,6 +6,6 @@ import { authLimiter } from "../middleware/limiter.js";
 const router = express.Router();
 
 router.post("/register", authLimiter, upload.single("picture"), register);
-router.post("/login", login);
+router.post("/login", authLimiter, login);
 
 export default router;
